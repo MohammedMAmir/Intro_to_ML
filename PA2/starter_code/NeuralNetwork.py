269cd3376e7a3b1fa7cb7a379c41b243b1a9bf6c
Mohammed Amir <muji-amir@hotmail.com>
2024-03-07 12:16:21 -0500

    Starter sklearn work for PA2

--- PA2/starter_code/NeuralNetwork.py
+++ PA2/starter_code/NeuralNetwork.py
@@ -24,7 +24,10 @@ def updateWeights(weights,g,alpha):
     #Enter implementation here
 
 def activation(s):
-    #Enter implementation here
+    if s < 0:
+        return 0
+    else:
+        return s
 
 def derivativeActivation(s):
     #Enter implementation here
@@ -45,13 +48,54 @@ def pred(x_n,weights):
     #Enter implementation here
     
 def confMatrix(X_train,y_train,w):
-    #Enter implementation here
+    N = X_train.shape[0]
+    #Create a column of ones and append it to X_train
+    ones = np.ones((N,1))
+    modified_X_train = np.hstack((ones, X_train))
+
+    #Initialize counts:
+    true_neg = 0
+    true_pos = 0
+    false_neg = 0
+    false_pos = 0
+
+    #Go through all the samples
+    for i in range(N):
+        #If y_i is 1:
+        if y_train[i] == 1:
+            #If the perceptron correctly outputs 1, increase true_pos
+            if(pred(modified_X_train[i], w) == y_train[i]):
+                true_pos += 1
+            #else, increase false_pos
+            else:
+                false_pos += 1
+        #If y_i is -1:
+        else:
+            #If the perceptron correctly outputs -1, increase true_neg
+            if(pred(modified_X_train[i], w) == y_train[i]):
+                true_neg += 1
+            #else, increase false_neg
+            else:
+                false_neg += 1
+    
+    #Create a two by two confusion matrix and fill it with respective counts
+    result = np.arange(4).reshape(2,2)
+    result[0][0] = true_neg
+    result[0][1] = false_pos
+    result[1][0] = false_neg
+    result[1][1] = true_pos
+    return result
 
 def plotErr(e,epochs):
     #Enter implementation here
+    stub = 0
+    return stub
     
 def test_SciKit(X_train, X_test, Y_train, Y_test):
-    #Enter implementation here
+    pct = MLPClassifier(solver="adam", alpha=1e-5, hidden_layer_sizes=(30,10), random_state=1)
+    pct.fit(X_train, Y_train)
+    confusion_matrix(Y_test, pct.predict(X_test))
+
 
 def test_Part1():
     from sklearn.datasets import load_iris
